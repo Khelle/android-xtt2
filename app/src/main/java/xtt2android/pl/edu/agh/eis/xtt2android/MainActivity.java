a6469ea8e21043e029c88dff2e55d199c6c5f6a4
khelle <jamroz.kamil@gmail.com>
2015-11-21 16:15:03 +0100

    Added navigation spinner

--- app/src/main/java/xtt2android/pl/edu/agh/eis/xtt2android/MainActivity.java
+++ app/src/main/java/xtt2android/pl/edu/agh/eis/xtt2android/MainActivity.java
@@ -2,6 +2,11 @@ package xtt2android.pl.edu.agh.eis.xtt2android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import heart.xtt.XTTModel;
 import xtt2android.pl.edu.agh.eis.xtt2android.hmr.XTT2Extractor;
@@ -15,6 +20,38 @@ public class MainActivity extends AppCompatActivity {
 
         XTT2Extractor extractor = new XTT2Extractor();
         XTTModel model = extractor.getXTTModel(getApplicationContext().getAssets());
+
+        final String[] items = new String[] {
+            "Option #1", "Option #2", "Option #3", "Insanely good option", "Option #5"
+        };
+
+        final Spinner s = (Spinner) findViewById(R.id.nav_sel_tab);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_spinner_item,
+            items
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Get select item
+                int sid = s.getSelectedItemPosition();
+                Toast.makeText(
+                    getBaseContext(),
+                    "You have selected City : " + items[sid],
+                    Toast.LENGTH_SHORT
+                ).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 
 }
