01ddabdd22e5ae11b0ae76627c6ca6994139b834
Michał Kurzeja <mike.kurzeja@gmail.com>
2015-11-21 18:09:25 +0100

    Rules List view WIP

--- app/src/main/java/xtt2android/pl/edu/agh/eis/xtt2android/MainActivity.java
+++ app/src/main/java/xtt2android/pl/edu/agh/eis/xtt2android/MainActivity.java
@@ -2,6 +2,8 @@ package xtt2android.pl.edu.agh.eis.xtt2android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -9,9 +11,13 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import heart.xtt.XTTModel;
+import xtt2android.pl.edu.agh.eis.xtt2android.adapter.RulesListAdapter;
 import xtt2android.pl.edu.agh.eis.xtt2android.hmr.XTT2Extractor;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRulesRecyclerView;
+    private RecyclerView.LayoutManager mRulesLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,28 @@ public class MainActivity extends AppCompatActivity {
 
         XTT2Extractor extractor = new XTT2Extractor();
         XTTModel model = extractor.getXTTModel(getApplicationContext().getAssets());
+
+        mRulesRecyclerView = (RecyclerView) findViewById(R.id.rules_recycler_view);
+        mRulesRecyclerView.setHasFixedSize(true);
+
+        mRulesLayoutManager = new LinearLayoutManager(this);
+        mRulesRecyclerView.setLayoutManager(mRulesLayoutManager);
+
+        String[] dataset = new String[] {
+                "Data1",
+                "Data2",
+                "Data3",
+                "Data4",
+                "Data5",
+                "Data6",
+                "Data7",
+                "Data8",
+                "Data9",
+                "Data10",
+        };
+
+        RulesListAdapter rulesAdapter = new RulesListAdapter(model.getTables().get(2));
+        mRulesRecyclerView.setAdapter(rulesAdapter);
 
         final String[] items = new String[] {
             "Option #1", "Option #2", "Option #3", "Insanely good option", "Option #5"
---
+++ app/src/main/java/xtt2android/pl/edu/agh/eis/xtt2android/adapter/RulesListAdapter.java
@@ -0,0 +1,95 @@
+package xtt2android.pl.edu.agh.eis.xtt2android.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+
+import heart.alsvfd.Formulae;
+import heart.xtt.Rule;
+import heart.xtt.Table;
+import xtt2android.pl.edu.agh.eis.xtt2android.R;
+
+public class RulesListAdapter extends RecyclerView.Adapter<RulesListAdapter.ViewHolder> {
+    final private String KEYWORD_IF = "IF";
+    final private String KEYWORD_AND = "AND";
+
+    private Rule[] mRules;
+
+    public RulesListAdapter(Table table) {
+        LinkedList<Rule> rules = table.getRules();
+        mRules = table.getRules().toArray(new Rule[rules.size()]);
+    }
+
+    @Override
+    public RulesListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        CardView v = (CardView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.view_rule_card, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Rule rule = mRules[position];
+        LinearLayout layout = (LinearLayout) holder.mCardView.findViewById(R.id.card_layout);
+        Context context = layout.getContext();
+
+        layout.addView(createKeywordTextView(context, KEYWORD_IF));
+
+        for (Formulae formulae : rule.getConditions()) {
+            layout.addView(createTextView(context, formulae.getAttribute().toString()));
+            layout.addView(createOperatorTextView(context, formulae.getOp().toString()));
+            layout.addView(createTextView(context, formulae.getValue().toString()));
+
+            layout.addView(createKeywordTextView(context, KEYWORD_AND));
+        }
+    }
+
+    private TextView createOperatorTextView(Context context, String text) {
+        TextView view = createTextView(context, text);
+        view.setTypeface(null, Typeface.ITALIC);
+
+        return view;
+    }
+
+    private TextView createKeywordTextView(Context context, String text) {
+        TextView view = createTextView(context, text);
+        view.setTypeface(null, Typeface.ITALIC);
+        view.setTextColor(Color.parseColor("#228f00"));
+
+        return view;
+    }
+
+    private TextView createTextView(Context context, String text) {
+        TextView view = new TextView(context);
+        view.setText(text);
+        view.setPadding(0, 0, 10, 0);
+
+        return view;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRules.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public CardView mCardView;
+
+        public ViewHolder(CardView v) {
+            super(v);
+            mCardView = v;
+        }
+    }
+}
